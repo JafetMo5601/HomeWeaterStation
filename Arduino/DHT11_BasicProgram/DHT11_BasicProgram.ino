e9a1aa8814ed194a1c8159f4d965441f8a9cabf4
jmo.py <jafet.moraugalde15@gmail.com>
2021-04-17 21:34:23 -0600

    Test a new program for the DHT11 and check that everything is working.

--- Arduino/DHT11_BasicProgram/DHT11_BasicProgram.ino
+++ Arduino/DHT11_BasicProgram/DHT11_BasicProgram.ino
@@ -1,18 +1,28 @@
-#include <dht.h>
+#include "DHT.h"
 
-dht DHT;
+DHT dht;
 
-#define DHT11_PIN 7
-
-void setup(){
+void setup()
+{
   Serial.begin(9600);
+  Serial.println();
+  Serial.println("Status\tHumidity (%)\tTemperature (C)\t(F)");
+  dht.setup(2);
 }
 
-void loop(){
-  int chk = DHT.read11(DHT11_PIN);
-  Serial.print("Temperature = ");
-  Serial.println(DHT.temperature);
-  Serial.print("Humidity = ");
-  Serial.println(DHT.humidity);
-  delay(1000);
+void loop()
+{
+  delay(dht.getMinimumSamplingPeriod());
+
+  float humidity = dht.getHumidity();   
+  float temperature = dht.getTemperature();
+
+  Serial.print(dht.getStatusString());    
+  Serial.print("\t");
+  Serial.print(humidity, 1);
+  Serial.print("\t\t");
+  Serial.print(temperature, 1);
+  Serial.print("\t\t");
+  Serial.println(dht.toFahrenheit(temperature), 1);
+  delay(2000);  
 }
